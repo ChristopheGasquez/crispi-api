c399c65bcb9d0319a67e60f054f6465ad3946dc0
gasquez.christophe <c.gasquez.dl@gmail.com>
2022-07-03 16:07:10 +0200

    Updated ensure-right-level.middleware.js => Replaced chains of promise by async/await function.

--- src/middlewares/ensure-right-level.middleware.js
+++ src/middlewares/ensure-right-level.middleware.js
@@ -4,40 +4,40 @@ import { Right } from '../schemas/right.schema.js';
 
 export default (level) => {
   // Return middleware.
-  return (req, res, next) => {
+  return async (req, res, next) => {
     // If header property exist.
     if (req?.issuer?.right) {
       // Get issuer right.
-      Right
-        .findById(req.issuer.right)
-        .then((right) => {
-          // If issue have level unless level passed on parameter.
-          if (right.level <= level) {
-            next();
-          }
-          // If issue have level higher level passed on parameter.
-          else {
-            // Create response with appropriated error.
-            const response = new Response(CONST.response.error.ensure.right.level);
-            // Set meta.
-            response.meta = {
-              reason: `You do not have access to this resource: insufficient rights`,
-            };
-            // Send response.
-            response.send(res);
-          }
-        })
-        .catch(() => {
-          // Create response.
-          const response = new Response(CONST.response.error.notFound);
+      try {
+        // Get issuer right.
+        const right = await Right.findById(req.issuer.right);
+        // If issue have level unless level passed on parameter.
+        if (right.level <= level) {
+          return next();
+        }
+        // If issue have level higher level passed on parameter.
+        else {
+          // Create response with appropriated error.
+          const response = new Response(CONST.response.error.ensure.right.level);
           // Set meta.
           response.meta = {
-            field: 'Authorization',
-            reason: 'Error: Right not found.'
+            reason: `You do not have access to this resource: insufficient rights`,
           };
           // Send response.
-          response.send(res);
-        });
+          return response.send(res);
+        }
+      }
+      catch {
+        // Create response.
+        const response = new Response(CONST.response.error.notFound);
+        // Set meta.
+        response.meta = {
+          field: 'Authorization',
+          reason: 'Error: Right not found.'
+        };
+        // Send response.
+        return response.send(res);
+      }
     }
     else {
       // Create response with appropriated error.
